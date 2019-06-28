07f53dbef4b61afca0ac609b7bb8ecbf538643e5
Narnia <software_good@163.com>
2019-06-28 21:35:10 +0800

    整合security

---
+++ src/main/java/com/example/eurekaserver/demo/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.eurekaserver.demo;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 新版的security默认启用了csrf检验，要在eureka服务端配置security的csrf检验为false
+ * @author lhx
+ * @date 2019/6/23
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
